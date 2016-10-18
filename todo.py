e27189b19dd2ae684542aac520e19887591ab6eb
okocsis90 <o.kocsis90@gmail.com>
2016-10-18 10:37:58 +0200

    working list

--- todo.py
+++ todo.py
@@ -3,6 +3,11 @@
 def list():
     open_list = open("todos.txt", "r")
     for line in open_list:
-        print(line, end="")
+        line = line.split(";")
+        if len(line) > 1:
+            if line[1][0] == "0":
+                print("[ ]", line[0])
+            elif line[1][0] == "1":
+                print("[x]", line[0])
 
-list()    
+list()            
